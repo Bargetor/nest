7373582224ca94879a1b5226e03e173d119510b1
Bargetor <bargetor@gmail.com>
2016-04-07 00:21:27 +0800

    1.重新定义storage manage interface，为优化做准备

--- src/com/bargetor/nest/storage/StorageManager.java
+++ src/com/bargetor/nest/storage/StorageManager.java
@@ -1,9 +1,27 @@
 package com.bargetor.nest.storage;
 
+import java.util.Collection;
+
 /**
  * Created by Bargetor on 16/4/3.
  */
 public interface StorageManager<T> {
-    public void save(T product);
-    public T get();
+    /**
+     * 是否可以进行生产
+     * @return
+     */
+    public boolean canProduce();
+
+    /**
+     * 获取一定数量的产品
+     * @param count
+     * @return
+     */
+    public Collection<T> getPorduce(int count);
+
+    /**
+     * 获取剩余产品数量
+     * @return
+     */
+    public int getProduceRemainingAmount();
 }
