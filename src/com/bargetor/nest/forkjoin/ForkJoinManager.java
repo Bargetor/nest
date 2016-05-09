95e78781a905c9731d8e45611db9e0cabae566c3
Bargetor <bargetor@gmail.com>
2016-05-09 23:46:55 +0800

    1.优化forkjoin task的实现

--- src/com/bargetor/nest/forkjoin/ForkJoinManager.java
+++ src/com/bargetor/nest/forkjoin/ForkJoinManager.java
@@ -1,16 +1,21 @@
 package com.bargetor.nest.forkjoin;
 
 import com.bargetor.nest.common.springmvc.SpringApplicationUtil;
+import com.bargetor.nest.common.util.ArrayUtil;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.InitializingBean;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
 
 /**
  * Created by Bargetor on 16/5/6.
  */
 public class ForkJoinManager implements InitializingBean {
+    private static final Logger logger = Logger.getLogger(ForkJoinManager.class);
     private static ForkJoinManager instance;
 
     private ForkJoinPool forkJoinPool;
@@ -46,7 +51,15 @@ public class ForkJoinManager implements InitializingBean {
      */
     public <I, R>List<R> parallelTask(Collection<I> inputs, ParallelTask.Worker<I, R> worker){
         ParallelTask<I, R> parallelTask = new ParallelTask<>(inputs, worker);
-        return this.forkJoinPool.invoke(parallelTask);
+        List<Future<R>> future = this.forkJoinPool.invokeAll(parallelTask.getWorkers());
+        return ArrayUtil.list2List(future, one -> {
+            try {
+                return one.get();
+            } catch (InterruptedException | ExecutionException e) {
+                logger.error(e);
+                return null;
+            }
+        });
     }
 
 
--- src/com/bargetor/nest/forkjoin/ParallelTask.java
+++ src/com/bargetor/nest/forkjoin/ParallelTask.java
@@ -5,12 +5,13 @@ import com.bargetor.nest.common.util.ArrayUtil;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.RecursiveTask;
 
 /**
  * Created by Bargetor on 16/5/6.
  */
-public class ParallelTask<I, R> extends RecursiveTask<List<R>> {
+public class ParallelTask<I, R>{
     private Collection<I> input;
     private Worker<I, R> worker;
 
@@ -20,29 +21,20 @@ public class ParallelTask<I, R> extends RecursiveTask<List<R>> {
         this.worker = worker;
     }
 
-    @Override
-    protected List<R> compute() {
+    public List<ParallelTaskWorker<I, R>> getWorkers() {
         if(ArrayUtil.isCollectionNull(this.input))return null;
         List<ParallelTaskWorker<I, R>> parallelTaskWorkers = new ArrayList<>();
-        List<R> result = new ArrayList<>();
 
         this.input.forEach(one -> {
             ParallelTaskWorker<I, R> parallelTaskWorker = new ParallelTaskWorker<>(one, this.worker);
-            parallelTaskWorker.fork();
             parallelTaskWorkers.add(parallelTaskWorker);
         });
 
-        parallelTaskWorkers.forEach(one -> {
-            R subResult = one.join();
-            if(subResult == null)return;
-            result.add(subResult);
-        });
-
-        return result;
+        return parallelTaskWorkers;
     }
 
 
-    class ParallelTaskWorker<I, R> extends RecursiveTask<R>{
+    class ParallelTaskWorker<I, R> implements Callable<R> {
         private I input;
         private Worker<I, R> worker;
 
@@ -52,7 +44,7 @@ public class ParallelTask<I, R> extends RecursiveTask<List<R>> {
         }
 
         @Override
-        protected R compute() {
+        public R call() throws Exception {
             return this.worker.work(this.input);
         }
     }
