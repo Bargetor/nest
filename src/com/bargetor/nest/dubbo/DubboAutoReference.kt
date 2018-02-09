5aae095d689b63a03982f7ec5986d17073a475ed
马进 <bargetor@gmail.com>
2018-02-10 00:12:38 +0800

    1.废弃dubbo auto reference

--- src/com/bargetor/nest/dubbo/DubboAutoReference.kt
+++ src/com/bargetor/nest/dubbo/DubboAutoReference.kt
@@ -5,6 +5,7 @@ import com.alibaba.dubbo.config.ReferenceConfig
 /**
  * Created by bargetor on 2017/11/4.
  */
+@Deprecated("破坏了dubbo的断线重连机制，故废弃")
 class DubboAutoReference<T>{
     var referenceConfig: ReferenceConfig<T>? = null
 
