870fb95bf884f89fcf2bc85cb52dfece5e7540ac
马进 <bargetor@gmail.com>
2018-02-11 12:42:02 +0800

    1.lock bug fixed

--- src/com/bargetor/nest/redis/RedissonLockHelper.kt
+++ src/com/bargetor/nest/redis/RedissonLockHelper.kt
@@ -6,24 +6,23 @@ import java.util.concurrent.TimeUnit
 
 class RedissonLockHelper{
     companion object {
-        fun lock(key: String, waitTime: Long, leaseTime: Long, onLock: () -> Unit, onLockGetFail: () -> Unit, onLockOccupied: () -> Unit){
+        fun lock(key: String, waitTime: Long, leaseTime: Long, onLock: () -> Unit, onLockGetFail: () -> Unit? = {}, onLockOccupied: () -> Unit? = {}): Unit{
             val redissonClient = SpringApplicationUtil.getBean(RedissonClient::class.java) as RedissonClient
             val lock = redissonClient.getLock(key)
             var locked = false
 
             try {
                 locked = lock.tryLock(waitTime, leaseTime, TimeUnit.MILLISECONDS)
+                if (locked) {
+                    onLock()
+                } else {
+                    onLockOccupied()
+                }
             } catch (e: InterruptedException) {
                 //获取锁失败，直接退出
                 onLockGetFail()
-            }
-
-
-            if (locked) {
-                onLock()
+            } finally {
                 lock.unlock()
-            } else {
-                onLockOccupied()
             }
         }
     }
