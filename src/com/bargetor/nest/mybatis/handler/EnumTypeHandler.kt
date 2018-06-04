373e92a2e17c85b94dadaae0e619b09ee6a84b4b
马进 <bargetor@gmail.com>
2018-06-05 00:29:25 +0800

    1.代码清理

--- src/com/bargetor/nest/mybatis/handler/EnumTypeHandler.kt
+++ src/com/bargetor/nest/mybatis/handler/EnumTypeHandler.kt
@@ -4,7 +4,6 @@ import org.apache.ibatis.type.EnumTypeHandler
 import org.apache.ibatis.type.JdbcType
 import org.apache.ibatis.type.MappedJdbcTypes
 import org.apache.ibatis.type.MappedTypes
-import sun.jvm.hotspot.debugger.cdbg.EnumType
 import java.sql.CallableStatement
 import java.sql.PreparedStatement
 import java.sql.ResultSet
