669ec8223bd31aa97e05be23fbffb3860c03f08d
马进 <bargetor@gmail.com>
2018-06-12 00:01:19 +0800

    1.string emoji filter

--- src/com/bargetor/nest/common/util/SystemUtil.kt
+++ src/com/bargetor/nest/common/util/SystemUtil.kt
@@ -1,10 +1,22 @@
 package com.bargetor.nest.common.util
 
-import com.google.common.collect.Range
+import java.util.regex.Pattern
 
 @kotlin.jvm.JvmName("systemPrintRunTime")
 public fun printRunTime(action: () -> Unit): Unit {
     val start = System.currentTimeMillis()
     action()
     println("run time:" + (System.currentTimeMillis() - start))
+}
+
+@kotlin.jvm.JvmName("filterEmoji")
+public fun String.filterEmoji(): String {
+    if (this.trim().isEmpty()) {
+        return this
+    }
+    val pattern = "[\ud83c\udc00-\ud83c\udfff]|[\ud83d\udc00-\ud83d\udfff]|[\u2600-\u27ff]"
+    val reStr = ""
+    val emoji = Pattern.compile(pattern)
+    val emojiMatcher = emoji.matcher(this)
+    return emojiMatcher.replaceAll(reStr)
 }
